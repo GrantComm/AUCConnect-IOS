d5c2af035ad526c85c617e0b9d0761e0f39646fd
Grant Commodore <grantcommodore@Grants-MacBook-Air.local>
2020-01-15 13:46:24 -0500

    Added User's Name on profile view

--- AUCConnect/profileViewController.swift
+++ AUCConnect/profileViewController.swift
@@ -12,16 +12,19 @@ import FirebaseAuth
 import FirebaseDatabase
 class profileViewController: UIViewController {
   //variables
-    @IBOutlet weak var userName: UILabel!
+
     
+    @IBOutlet weak var firstName: UILabel!
+    @IBOutlet weak var lastName: UILabel!
     @IBOutlet weak var profilePic: UIImageView!
     
+    @IBOutlet weak var schoolBackground: UIImageView!
     @IBOutlet weak var roundedLogout: UIButton!
     
     var user = Auth.auth().currentUser;
     var ref: DatabaseReference!
     
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -32,20 +35,53 @@ class profileViewController: UIViewController {
         roundedLogout.layer.cornerRadius = 10
         roundedLogout.contentEdgeInsets = UIEdgeInsets(top: 5,left: 5,bottom: 5,right: 5)
         // Do any additional setup after loading the view.
-        if (user != nil) {
-            print("Hello World")
+        let user = Auth.auth().currentUser;
+        if let user = user{
+            //Properties of the Current User
+            let uid = user.uid
+            let email = user.email
+            let domainMH = "morehouse"
+            let domainSP = "spelman"
+            let domainCL = "cau"
+            //Access the name of the user
+            var delimiter1 = "@"
+            var delimiter2 = "."
+            var newstr = email!
+            var token = newstr.components(separatedBy: delimiter1)
+            var token2 = token[0].components(separatedBy: delimiter2)
+            firstName.text = token2[0].uppercased()
+            lastName.text = token2[1].uppercased()
+            //Change Background color based on school 
+            if(self.whichColor(testEmail: email!, domain: domainMH) == true){
+                print("HELLO MH USER")
+            }
+            if(self.whichColor(testEmail: email!, domain: domainSP) == true){
+                print("HELLO SP USER")
+            }
+            if(self.whichColor(testEmail: email!, domain: domainCL) == true){
+                print("HELLO CL USER")
+            }
         }
     }
     
     
+    
+    func whichColor(testEmail:String, domain:String) -> Bool {
+        
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[\(domain)]+\\.[edu]{3,\(domain.count)}"
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        let result = emailTest.evaluate(with: testEmail)
+        return result
+        
+    }
+    
     @IBAction func signout(_ sender: Any) {
-        if Auth.auth().currentUser != nil {
+        if user != nil {
             do {
                 try Auth.auth().signOut()
                 self.performSegue(withIdentifier: "signinView", sender: self)
-
-                
             } catch let error as NSError {
+                print("Hello There")
                 print(error.localizedDescription)
             }
         }
